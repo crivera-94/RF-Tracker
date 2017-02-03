b2781eb03405a8ef2976b3f7adea0c6737bcf6cd
“Carlos <carlos.rivera.ece@gmail.com>
2017-02-02 17:50:31 -0800

    added adafruit library

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -10,7 +10,8 @@ import ADS1x15
 
 
 # Create an ADS1115 ADC (16-bit) instance.
-adc = Adafruit_ADS1x15.ADS1115()
+#adc = Adafruit_ADS1x15.ADS1115()
+adc = ADS1x15.ADS1115()
 
 # Or create an ADS1015 ADC (12-bit) instance.
 #adc = Adafruit_ADS1x15.ADS1015()
