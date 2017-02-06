3fa9b53175793c3cb77678a9e2c13e1ab8793943
“Carlos <carlos.rivera.ece@gmail.com>
2017-02-05 20:00:19 -0800

    changed button dimensions

--- mainwindow_auto.py
+++ mainwindow_auto.py
@@ -27,7 +27,7 @@ class Ui_MainWindow(object):
         self.btnOff.setGeometry(QtCore.QRect(220, 90, 90, 45))
         self.btnOff.setObjectName("btnOff")
         self.label = QtWidgets.QLabel(self.centralWidget)
-        self.label.setGeometry(QtCore.QRect(150, 50, 90, 20))
+        self.label.setGeometry(QtCore.QRect(130, 50, 90, 20))
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         MainWindow.setCentralWidget(self.centralWidget)
