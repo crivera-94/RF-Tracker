55f2ba49800a3b51f7b25b4d1250a2c09e6f7370
“Carlos <carlos.rivera.ece@gmail.com>
2017-02-28 20:06:37 -0800

    fixed indent

--- samples/concentriccircles.py
+++ samples/concentriccircles.py
@@ -42,7 +42,7 @@
 #############################################################################
 
 
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QWidget
 
 class CircleWidget(QWidget):
     def __init__(self, parent=None):
