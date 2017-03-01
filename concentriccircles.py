0abd508d3fabcb16205dd1c84aeb87c90fca76fa
“Carlos <carlos.rivera.ece@gmail.com>
2017-02-28 20:10:00 -0800

    fixed indent

--- concentriccircles.py
+++ concentriccircles.py
@@ -77,37 +77,39 @@ class CircleWidget(QWidget):
         self.frameNo += 1
         self.update()
     
-    #def paintEvent(self, event):
-        #painter = QPainter(self)
-        #painter.setRenderHint(QPainter.Antialiasing, self.antialiased)
-        #painter.translate(self.width() / 2, self.height() / 2)
-        
-        #for diameter in range(0, 256, 9):
-            #delta = abs((self.frameNo % 128) - diameter / 2)
-            #alpha = 255 - (delta * delta) / 4 - diameter
-            #if alpha > 0:
-                #painter.setPen(QPen(QColor(0, diameter / 2, 127, alpha), 3))
-                
-                #if self.floatBased:
-                    #painter.drawEllipse(QRectF(-diameter / 2.0,
-                                                #-diameter / 2.0, diameter, diameter))
-                #else:
-                    #painter.drawEllipse(QRect(-diameter / 2,
-                                                #-diameter / 2, diameter, diameter))
-
     def paintEvent(self, event):
-        color = QColor(0, 0, 0)
-        color.setNamedColor('#4080fe')
-        
         painter = QPainter(self)
-        painter.setPen(color)
         painter.setRenderHint(QPainter.Antialiasing, self.antialiased)
         painter.translate(self.width() / 2, self.height() / 2)
         
-        for diameter in range(0, 256, 20):
-            delta = abs((40 % 128) - diameter / 2)
+        for diameter in range(0, 256, 9):
+            delta = abs((self.frameNo % 128) - diameter / 2)
             alpha = 255 - (delta * delta) / 4 - diameter
-            painter.drawEllipse(QRectF(-diameter / 2.0, -diameter / 2.0, diameter, diameter))
+            if alpha > 0:
+                painter.setPen(QPen(QColor(0, diameter / 2, 127, alpha), 3))
+                
+                if self.floatBased:
+                    painter.drawEllipse(QRectF(-diameter / 2.0,
+                                                -diameter / 2.0, diameter, diameter))
+                else:
+                    painter.drawEllipse(QRect(-diameter / 2,
+                                                -diameter / 2, diameter, diameter))
+
+    #def paintEvent(self, event):
+        #color = QColor(0, 0, 0)
+        #color.setNamedColor('#4080fe')
+        
+        #painter = QPainter(self)
+        #painter.setPen(color)
+        #painter.setRenderHint(QPainter.Antialiasing, self.antialiased)
+        #painter.translate(self.width() / 2, self.height() / 2)
+        
+        #for diameter in range(0, 256, 20):
+            #delta = abs((40 % 128) - diameter / 2)
+            #alpha = 255 - (delta * delta) / 4 - diameter
+            #painter.drawEllipse(QRectF(-diameter / 2.0, -diameter / 2.0, diameter, diameter))
+
+
             #if alpha > 0:
                 #painter.setPen(QPen(QColor(0, diameter / 2, 127, alpha), 3))
                 
