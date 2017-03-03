97fb8204efecdf4e848e1282645d64e7984a4a69
“Carlos <carlos.rivera.ece@gmail.com>
2017-03-03 14:45:41 -0800

    testing thread

--- samples/base.py
+++ samples/base.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
         thread.start_new_thread( print_time, ("Thread-1", 2, ) )
         thread.start_new_thread( print_time, ("Thread-2", 4, ) )
     except:
-        print "Error: unable to start thread"
+        print("Error: unable to start thread")
     
     app = QApplication(sys.argv)
     ex = RFTracker()
