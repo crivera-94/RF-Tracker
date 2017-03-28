52c529bb64c773b9935f6c8ed5a637dc7f7d7331
“Carlos <carlos.rivera.ece@gmail.com>
2017-03-28 15:04:27 -0700

    added amplitudes and phases

--- samples/threadpackage.py
+++ samples/threadpackage.py
@@ -3,16 +3,29 @@ from PyQt5.QtCore import QThread
 from phasedetector import PhaseDetector
 from pykalman import KalmanFilter
 
-amplitude = 0
-phase = 0
+# antenna 1 - 2
+amplitude1 = 0
+phase1 = 0
+
+# antenna 2 - 3
+amplitude2 = 0
+phase2 = 0
+
+# antenna 1 - 3
+amplitude3 = 0
+phase3 = 0
 
 
 class ADCThread(QThread):
 
     def __init__(self):
         QThread.__init__(self)
-        global amplitude
-        global phase
+        global amplitude1
+        global phase1
+        global amplitude2
+        global phase2
+        global amplitude3
+        global phase3
         self.phase_detector0 = PhaseDetector(48)
         self.phase_detector1 = PhaseDetector(52)
 
