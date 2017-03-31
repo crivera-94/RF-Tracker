08cb79ccb637f54484a3d33fc002bd2de0167391
“Carlos <carlos.rivera.ece@gmail.com>
2017-03-31 12:49:01 -0700

    modularized globals

---
+++ samples/globals.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import QMutex
+from enum import Enum
+
+
+class Sector(Enum):
+    A = 0
+    B = 1
+    C = 2
+
+
+def init():
+    global amplitudeA
+    amplitudeA = 0
+    global amplitudeB
+    amplitudeB = 0
+    global amplitudeC
+    amplitudeC = 0
+    global mutex
+    mutex = QMutex()
+    global resultant_angle
+    resultant_angle = 0
+    global resultant_sector
+    resultant_sector = Sector.A
+    global global_x
+    global_x = 0
+    global global_y
+    global_y = 0
