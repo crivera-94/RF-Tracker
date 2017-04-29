bc0ad974ede6ff17d48d3d46d830c07f8a665905
“Carlos <carlos.rivera.ece@gmail.com>
2017-04-28 22:10:05 -0700

    adding update code to database thread

--- samples/threadpackage.py
+++ samples/threadpackage.py
@@ -45,7 +45,8 @@ class DatabaseThread(QThread):
     def run(self):
         while True:
             if self.setup:
-                self.setup = True
+                globals.database.child("users").child("coordinates").update({"amplitude": self.data['amplitude']}, globals.user_token)
+                globals.database.child("users").child("coordinates").update({"distance": self.data['distance']}, globals.user_token)
                 #print("Posting...")
                 #self.data['amplitude'] = globals.global_amplitude
                 #self.data['distance'] = globals.global_distance
